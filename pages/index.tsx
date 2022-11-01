d59ddf8b144eba1ce5ff35011151119a15bc35c5
Freedisch <82499435+Freedisch@users.noreply.github.com>
2022-11-01 18:03:36 +0200

    solved eslint error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,15 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+// import Head from "next/head";
+// import Image from "next/image";
+// import styles from "../styles/Home.module.css";
 import Typewriter from "typewriter-effect";
 
 export default function Home() {
   return (
     <div className="grid items-center justify-center h-screen">
       <div className="">
-        <h1 className="font-mono"> Hello, I'm</h1>
+        <h1 className="font-mono"> Hello, I`m</h1>
         <h1 className="font-bold text-5xl">
-          <span className="text-lime-500">
+          <span className="text-sky-500">
             <Typewriter
               options={{
                 strings: ["Thibaut Freedisch", "Back-End engineer", "Student"],
@@ -20,9 +20,9 @@ export default function Home() {
           </span>
         </h1>
         <p className="text-2xl p-5">
-          Welcome to my portfolio, it's currently underdevelopment click
+          Welcome to my portfolio, currently underdevelopment click
           <a
-            className="text-purple-400"
+            className="text-sky-400"
             href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
           >
             {" "}
