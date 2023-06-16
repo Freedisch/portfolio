a17816bd522e4068f1e1b84992247f53d4e8f4b4
Freedisch <82499435+Freedisch@users.noreply.github.com>
2023-06-16 20:44:56 +0300

    update experience
    Signed-off-by: Freedisch <82499435+Freedisch@users.noreply.github.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -307,6 +307,162 @@ export default function Home() {
             <div>
               <div className="content-card my-5 px-4 py-5 border-2 border-blue-500 shadow-lg hover:border-purple-500 hover:shadow-2xl">
                 <div className="text-lg font-bold mb-1">
+                  Open Source Developer
+                </div>
+                <a
+                  href="https://owasp.org/www-project-juice-shop/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="linkstyle flex text-black text-sm w-fit mb-1 space-x-2"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                    className="w-4"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z"
+                    ></path>
+                  </svg>
+                  <div>OWASP Fundation</div>
+                </a>
+                <div className="flex flex-col-reverse sm:flex-row sm:justify-between text-sm text-gray-500">
+                  <div className=" flex items-center space-x-2 mb-1">
+                    <div>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                        className="w-4"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
+                        ></path>
+                      </svg>
+                    </div>
+                    <div>December 2022 - Present</div>
+                  </div>
+                </div>
+                <div className="text-sm mt-2 text-gray-600">
+                  <p className="my-1">
+                    - Migrated frontend to angular 15 🤖
+                    <a
+                      href="https://github.com/juice-shop/juice-shop/pull/1942"
+                      className="text-blue-600"
+                    >
+                      PR Link
+                    </a>
+                  </p>
+                  <p className="my-1">
+                    - Integrated fly fliter allowing users to filter to any
+                    challenges and fixed unit test error{" "}
+                    <a
+                      href="https://github.com/juice-shop/juice-shop/pull/1998"
+                      className="text-blue-600"
+                    >
+                      PR Link
+                    </a>
+                    .
+                  </p>
+                  <p className="my-1">
+                    - Added empty user registation challenge for ctf player.{" "}
+                    <a href="" className="text-blue-600">
+                      PR Link
+                    </a>
+                    .
+                  </p>
+                </div>
+              </div>
+
+              <div className="content-card my-5 px-4 py-5 border-2 border-blue-500 shadow-lg hover:border-purple-500 hover:shadow-2xl">
+                <div className="text-lg font-bold mb-1">
+                  Open Source Developer
+                </div>
+                <a
+                  href="https://meshery.io/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="linkstyle flex text-black text-sm w-fit mb-1 space-x-2"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                    className="w-4"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z"
+                    ></path>
+                  </svg>
+                  <div>Meshery</div>
+                </a>
+                <div className="flex flex-col-reverse sm:flex-row sm:justify-between text-sm text-gray-500">
+                  <div className=" flex items-center space-x-2 mb-1">
+                    <div>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                        className="w-4"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
+                        ></path>
+                      </svg>
+                    </div>
+                    <div>March 2023 - Present</div>
+                  </div>
+                </div>
+                <div className="text-sm mt-2 text-gray-600">
+                  <p className="my-1">
+                    - Working on changing api docs generation from swagger to
+                    redoclys{" "}
+                    <a
+                      href="https://github.com/meshery/meshery/pull/7534"
+                      className="text-blue-600"
+                    >
+                      PR Link
+                    </a>
+                  </p>
+                  <p className="my-1">
+                    - Working on fixing end2end cypress test.
+                    <a
+                      href="https://github.com/meshery/meshery/pull/7658"
+                      className="text-blue-600"
+                    >
+                      PR Link
+                    </a>
+                  </p>
+                  <p className="my-1">
+                    - Helped newcomers integrate the community. 🙂.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="content-card my-5 px-4 py-5 border-2 border-blue-500 shadow-lg hover:border-purple-500 hover:shadow-2xl">
+                <div className="text-lg font-bold mb-1">
                   Community Engager Volunteer
                 </div>
                 <a
@@ -351,7 +507,7 @@ export default function Home() {
                         ></path>
                       </svg>
                     </div>
-                    <div>March 2023 - Present</div>
+                    <div>March 2023 - June 2023</div>
                   </div>
                   <a
                     className="linkstyle w-fit flex space-x-2 items-center mb-1"
@@ -455,164 +611,6 @@ export default function Home() {
                   <p className="my-1">
                     - Build a docker container for each service and deploy a
                     Kubernetes cluster on digital ocean
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div id="experience">
-            <h2 className="mt-8 mb-4 text-2xl font-bold">
-              Open Source Contributions
-            </h2>
-            <div>
-              <div className="content-card my-5 px-4 py-5 border-2 border-blue-500 shadow-lg hover:border-purple-500 hover:shadow-2xl">
-                <div className="text-lg font-bold mb-1">Contributor</div>
-                <a
-                  href="https://owasp.org/www-project-juice-shop/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="linkstyle flex text-black text-sm w-fit mb-1 space-x-2"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                    className="w-4"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z"
-                    ></path>
-                  </svg>
-                  <div>OWASP Fundation</div>
-                </a>
-                <div className="flex flex-col-reverse sm:flex-row sm:justify-between text-sm text-gray-500">
-                  <div className=" flex items-center space-x-2 mb-1">
-                    <div>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        aria-hidden="true"
-                        className="w-4"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
-                        ></path>
-                      </svg>
-                    </div>
-                    <div>December 2022 - Present</div>
-                  </div>
-                </div>
-                <div className="text-sm mt-2 text-gray-600">
-                  <p className="my-1">
-                    - Migrated frontend to angular 15 🤖
-                    <a
-                      href="https://github.com/juice-shop/juice-shop/pull/1942"
-                      className="text-blue-600"
-                    >
-                      PR Link
-                    </a>
-                  </p>
-                  <p className="my-1">
-                    - Integrated fly fliter allowing users to filter to any
-                    challenges and fixed unit test error{" "}
-                    <a
-                      href="https://github.com/juice-shop/juice-shop/pull/1998"
-                      className="text-blue-600"
-                    >
-                      PR Link
-                    </a>
-                    .
-                  </p>
-                  <p className="my-1">
-                    - Added empty user registation challenge for ctf player.{" "}
-                    <a href="" className="text-blue-600">
-                      PR Link
-                    </a>
-                    .
-                  </p>
-                </div>
-              </div>
-
-              <div className="content-card my-5 px-4 py-5 border-2 border-blue-500 shadow-lg hover:border-purple-500 hover:shadow-2xl">
-                <div className="text-lg font-bold mb-1">Contributor</div>
-                <a
-                  href="https://meshery.io/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="linkstyle flex text-black text-sm w-fit mb-1 space-x-2"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                    className="w-4"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z"
-                    ></path>
-                  </svg>
-                  <div>Meshery</div>
-                </a>
-                <div className="flex flex-col-reverse sm:flex-row sm:justify-between text-sm text-gray-500">
-                  <div className=" flex items-center space-x-2 mb-1">
-                    <div>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        aria-hidden="true"
-                        className="w-4"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"
-                        ></path>
-                      </svg>
-                    </div>
-                    <div>March 2023 - Present</div>
-                  </div>
-                </div>
-                <div className="text-sm mt-2 text-gray-600">
-                  <p className="my-1">
-                    - Working on changing api docs generation from swagger to
-                    redoclys{" "}
-                    <a
-                      href="https://github.com/meshery/meshery/pull/7534"
-                      className="text-blue-600"
-                    >
-                      PR Link
-                    </a>
-                  </p>
-                  <p className="my-1">
-                    - Working on fixing end2end cypress test.
-                    <a
-                      href="https://github.com/meshery/meshery/pull/7658"
-                      className="text-blue-600"
-                    >
-                      PR Link
-                    </a>
-                  </p>
-                  <p className="my-1">
-                    - Helped newcomers integrate the community. 🙂.
                   </p>
                 </div>
               </div>
